3d325a37e8893711ec203ff92d8ea0e0fa28b401
YangDiA <1357145410@qq.com>
2017-07-26 09:56:02 +0800

    test

--- yangdi-ueditor-server-api/src/main/java/hello/Application.java
+++ yangdi-ueditor-server-api/src/main/java/hello/Application.java
@@ -17,8 +17,11 @@ import java.io.IOException;
 @SpringBootApplication
 @RestController
 public class Application {
-	@Value("${server.zimg.server:http://192.168.1.160:4869/upload}")
-	private  String zimgServer;
+	//@Value("${website.ueditor.imageUrlPrefix:http://192.168.1.160:4869/}")
+	String imageUrlPrefix="http://192.168.1.160:4869/";
+
+	//@Value("${server.zimg.server:http://192.168.1.160:4869/upload}")
+	private  String zimgServer="http://192.168.1.160:4869/upload";
 
 	@RequestMapping("/")
 	public String home() {
@@ -31,12 +34,17 @@ public class Application {
 		try {
 
 			String rootPath = request.getRealPath( "/" );
+			Map<String ,Object> mapParam = new HashMap<>();
+			mapParam.put("imageUrlPrefix",imageUrlPrefix);
+
+			// 必须初始化图片服务器地址
 			UploadImageToZimgUtil.getIstance(zimgServer);
-			response.getWriter().write(new ActionEnter( request,rootPath ).exec());
+			response.getWriter().write(new ActionEnter( request,rootPath,mapParam ).exec());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
